bfc92447a6399595f3b7f8047b81f8322add0eae
Duncan McGregor <duncan@oneeyedmen.com>
2021-10-18 11:33:00 +0100

    Introduced age and update2 methods

--- src/main/java/com/gildedrose/Item.kt
+++ src/main/java/com/gildedrose/Item.kt
@@ -15,28 +15,13 @@ open class BaseItem(
 ) : Item(name, sellIn, quality) {
 
     fun update() {
-        val name = name
         update1()
-        if (name != "Sulfuras, Hand of Ragnaros") {
-            sellIn = sellIn - 1
-        }
-        if (sellIn < 0) {
-            if (name != "Aged Brie") {
-                if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (quality > 0) {
-                        if (name != "Sulfuras, Hand of Ragnaros") {
-                            quality = quality - 1
-                        }
-                    }
-                } else {
-                    quality = quality - quality
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1
-                }
-            }
-        }
+        age()
+        update3()
+    }
+
+    protected open fun age() {
+        sellIn = sellIn - 1
     }
 
     protected open fun update1() {
@@ -44,16 +29,33 @@ open class BaseItem(
             quality = quality - 1
         }
     }
+
+    protected open fun update3() {
+        if (sellIn < 0) {
+            if (quality > 0) {
+                quality = quality - 1
+            }
+        }
+    }
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
     override fun update1() {}
+    override fun age() {}
+    override fun update3() {}
 }
 
 class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
     override fun update1() {
         if (quality < 50) {
             quality = quality + 1
+        }
+    }
+    override fun update3() {
+        if (sellIn < 0) {
+            if (quality < 50) {
+                quality = quality + 1
+            }
         }
     }
 }
@@ -70,6 +72,17 @@ class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, qua
             if (sellIn < 6) {
                 if (quality < 50) {
                     quality = quality + 1
+                }
+            }
+        }
+    }
+    override fun update3() {
+        if (sellIn < 0) {
+            if (true) {
+                quality = quality - quality
+            } else {
+                if (quality > 0) {
+                    quality = quality - 1
                 }
             }
         }
