36775478b0e9e2c1e463adc8cd72c801f227d434
Duncan McGregor <duncan@oneeyedmen.com>
2021-10-18 11:43:52 +0100

    Introduce saturate

--- src/main/java/com/gildedrose/Item.kt
+++ src/main/java/com/gildedrose/Item.kt
@@ -17,6 +17,7 @@ open class BaseItem(
     fun update() {
         age()
         degrade()
+        saturate()
     }
 
     protected open fun age() {
@@ -24,49 +25,41 @@ open class BaseItem(
     }
 
     protected open fun degrade() {
-        if (quality > 0) {
+        quality = quality - 1
+        if (sellIn < 0) {
             quality = quality - 1
         }
-        if (sellIn < 0) {
-            if (quality > 0) {
-                quality = quality - 1
-            }
-        }
+    }
+
+    protected open fun saturate() {
+        if (quality < 0) quality = 0
+        if (quality > 50) quality = 50
     }
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun degrade() {}
     override fun age() {}
+    override fun degrade() {}
+    override fun saturate() {}
 }
 
 class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
     override fun degrade() {
-        if (quality < 50) {
-            quality = quality + 1
-        }
+        quality = quality + 1
         if (sellIn < 0) {
-            if (quality < 50) {
-                quality = quality + 1
-            }
+            quality = quality + 1
         }
     }
 }
 
 class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
     override fun degrade() {
-        if (quality < 50) {
+        quality = quality + 1
+        if (sellIn < 10) {
             quality = quality + 1
-            if (sellIn < 10) {
-                if (quality < 50) {
-                    quality = quality + 1
-                }
-            }
-            if (sellIn < 5) {
-                if (quality < 50) {
-                    quality = quality + 1
-                }
-            }
+        }
+        if (sellIn < 5) {
+            quality = quality + 1
         }
         if (sellIn < 0) {
             quality = 0
