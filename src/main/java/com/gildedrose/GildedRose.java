93c3a14d850cbcdd8bfa0f5eedfc2a710619344d
Duncan McGregor <duncan@oneeyedmen.com>
2021-10-18 10:56:20 +0100

    Convert Item to Kotlin

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,51 +9,51 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+            if (!items[i].getName().equals("Aged Brie")
+                    && !items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].getQuality() > 0) {
+                    if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+                        items[i].setQuality(items[i].getQuality() - 1);
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (items[i].getQuality() < 50) {
+                    items[i].setQuality(items[i].getQuality() + 1);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items[i].getSellIn() < 11) {
+                            if (items[i].getQuality() < 50) {
+                                items[i].setQuality(items[i].getQuality() + 1);
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (items[i].getSellIn() < 6) {
+                            if (items[i].getQuality() < 50) {
+                                items[i].setQuality(items[i].getQuality() + 1);
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+                items[i].setSellIn(items[i].getSellIn() - 1);
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (items[i].getSellIn() < 0) {
+                if (!items[i].getName().equals("Aged Brie")) {
+                    if (!items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items[i].getQuality() > 0) {
+                            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+                                items[i].setQuality(items[i].getQuality() - 1);
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (items[i].getQuality() < 50) {
+                        items[i].setQuality(items[i].getQuality() + 1);
                     }
                 }
             }
--- src/main/java/com/gildedrose/Item.kt
+++ src/main/java/com/gildedrose/Item.kt
@@ -1,21 +1,9 @@
-package com.gildedrose;
+package com.gildedrose
 
-public class Item {
-
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
-
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
-
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
+class Item(
+    val name: String,
+    var sellIn: Int = 0,
+    var quality: Int = 0
+) {
+    override fun toString() = "$name, $sellIn, $quality"
 }
