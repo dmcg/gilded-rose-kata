4d1737931563b1fe71ca5cec344a4d27aba99716
Duncan McGregor <duncan@oneeyedmen.com>
2021-10-19 11:34:02 +0100

    Replace method overriding with function parameters

--- src/main/java/com/gildedrose/Item.kt
+++ src/main/java/com/gildedrose/Item.kt
@@ -12,7 +12,20 @@ open class BaseItem(
     name: String,
     sellIn: Int,
     quality: Int,
-    private val aging: () -> Int = { 1 }
+    private val aging: () -> Int = { 1 },
+    private val degradation: (Int, Int) -> Int = { sellIn: Int, quality: Int ->
+        when {
+            sellIn < 0 -> 2
+            else -> 1
+        }
+    },
+    private val saturation: (Int) -> Int = { quality: Int ->
+        when {
+            quality < 0 -> 0
+            quality > 50 -> 50
+            else -> quality
+        }
+    }
 ) : Item(name, sellIn, quality) {
 
     fun update() {
@@ -20,42 +33,44 @@ open class BaseItem(
         quality = saturation(quality - degradation(sellIn, quality))
     }
 
-    protected open fun degradation(sellIn: Int, quality: Int) = when {
-        sellIn < 0 -> 2
-        else -> 1
-    }
-
-    protected open fun saturation(quality: Int) = when {
-        quality < 0 -> 0
-        quality > 50 -> 50
-        else -> quality
-    }
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(
     name,
     sellIn,
     quality,
-    aging = { 0 }
+    aging = { 0 },
+    degradation = { _, _ -> 0 },
+    saturation = { it }
 ) {
-    override fun degradation(sellIn: Int, quality: Int) = 0
-    override fun saturation(quality: Int) = quality
 }
 
-class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun degradation(sellIn: Int, quality: Int) = when {
-        sellIn < 0 -> -2
-        else -> -1
+class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(
+    name,
+    sellIn,
+    quality,
+    degradation = { sellIn, _ ->
+        when {
+            sellIn < 0 -> -2
+            else -> -1
+        }
     }
+) {
 }
 
-class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun degradation(sellIn: Int, quality: Int) = when {
-        sellIn < 0 -> quality
-        sellIn < 5 -> -3
-        sellIn < 10 -> -2
-        else -> -1
+class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(
+    name,
+    sellIn,
+    quality,
+    degradation = { sellIn, quality ->
+        when {
+            sellIn < 0 -> quality
+            sellIn < 5 -> -3
+            sellIn < 10 -> -2
+            else -> -1
+        }
     }
+) {
 }
 
 
